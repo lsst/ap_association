6ba1d59bf3cf3a978d49078d0b060649a3372491
Kian-Tat Lim <ktl@slac.stanford.edu>
2022-01-25 21:09:08 -0800

    Use unittest.mock.patch instead of mock's.

--- tests/test_skyBotEphemerisQuery.py
+++ tests/test_skyBotEphemerisQuery.py
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from mock import patch
+from unittest.mock import patch
 import numpy as np
 import os
 import pandas as pd
