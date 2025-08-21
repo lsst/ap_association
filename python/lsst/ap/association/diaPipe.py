a60989da40c9292fc559c57d7e8639139c1914bf
Eric Bellm <ecbellm@uw.edu>
2025-08-21 10:59:26 -0700

    Use standardized and updated diaObjects in alert packaging.

--- python/lsst/ap/association/diaPipe.py
+++ python/lsst/ap/association/diaPipe.py
@@ -646,7 +646,7 @@ class DiaPipelineTask(pipeBase.PipelineTask):
                     inplace=True)
 
             self.alertPackager.run(assocResults.associatedDiaSources,
-                                   diaCalResult.updatedDiaObjects,
+                                   updatedDiaObjects,
                                    preloadedDiaSources,
                                    diaForcedSourcesFull,
                                    diffIm,
