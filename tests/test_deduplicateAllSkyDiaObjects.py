3dbcbfe897a8d5af282b341783f725d1e1b0b30a
Eric Bellm <ecbellm@uw.edu>
2026-06-03 09:37:15 -0700

    simplify unit tests

--- tests/test_deduplicateAllSkyDiaObjects.py
+++ tests/test_deduplicateAllSkyDiaObjects.py
@@ -27,7 +27,7 @@ import tempfile
 import unittest
 
 from lsst.ap.association import DeduplicateAllSkyDiaObjectsTask
-from lsst.ap.association.utils import getMidpointFromTimespan, readSchemaFromApdb
+from lsst.ap.association.utils import getMidpointFromTimespan
 from lsst.dax.apdb import Apdb, ApdbSql
 from lsst.utils import getPackageDir
 import lsst.utils.tests
@@ -54,121 +54,79 @@ def _data_file_name(basename, module_name):
 
 
 class TestDeduplicateAllSkyDiaObjects(unittest.TestCase):
+    """Tests for the full DeduplicateAllSkyDiaObjectsTask including
+    run(), remap_clusters(), and count_duplicates().
+    """
 
     def setUp(self):
-        # Create an instance of random generator with fixed seed.
+        self.task = _make_task(maxClusteringDistance=1.5)
+
+    def _make_apdb_with_duplicates(self):
+        """Set up an APDB with known duplicate DiaObjects and return
+        the task, expected duplicate IDs, and APDB access info.
+        """
         rng = np.random.default_rng(1234)
 
-        self.db_file_fd, self.db_file = tempfile.mkstemp(
-            dir=os.path.dirname(__file__))
-        self.addCleanup(os.remove, self.db_file)
-        self.addCleanup(os.close, self.db_file_fd)
+        db_file_fd, db_file = tempfile.mkstemp(dir=os.path.dirname(__file__))
+        self.addCleanup(os.remove, db_file)
+        self.addCleanup(os.close, db_file_fd)
 
-        self.apdbConfig = ApdbSql.init_database(db_url="sqlite:///" + self.db_file)
-        self.config_file = tempfile.NamedTemporaryFile()
-        self.addCleanup(self.config_file.close)
-        self.apdbConfig.save(self.config_file.name)
-        self.apdb = Apdb.from_config(self.apdbConfig)
-        self.schema = readSchemaFromApdb(self.apdb)
+        apdbConfig = ApdbSql.init_database(db_url="sqlite:///" + db_file)
+        config_file = tempfile.NamedTemporaryFile()
+        self.addCleanup(config_file.close)
+        apdbConfig.save(config_file.name)
+        apdb = Apdb.from_config(apdbConfig)
 
-        self.exposure = makeExposure(False, False)
-        self.regionTime = makeRegionTime(exposure=self.exposure)
-        self.dateTime = getMidpointFromTimespan(self.regionTime.timespan)
+        exposure = makeExposure(False, False)
+        regionTime = makeRegionTime(exposure=exposure)
+        dateTime = getMidpointFromTimespan(regionTime.timespan)
 
-        original_diaObjects = makeDiaObjects(50, self.exposure, rng)
+        original_diaObjects = makeDiaObjects(50, exposure, rng)
 
-        # create duplicates
-        dup_0_0 = {"ra": original_diaObjects.iloc[0]['ra'],
-                   "dec": original_diaObjects.iloc[0]['dec'] + 0.2/3600,
-                   "diaObjectId": 1000,
-                   "nDiaSources": 1}
-        for f in ["u", "g", "r", "i", "z", "y"]:
-            dup_0_0["%s_psfFluxNdata" % f] = 0
-        dup_0_1 = {"ra": original_diaObjects.iloc[0]['ra'] + 0.1/3600,
-                   "dec": original_diaObjects.iloc[0]['dec'] + 0.2/3600,
-                   "diaObjectId": 1001,
-                   "nDiaSources": 1}
-        for f in ["u", "g", "r", "i", "z", "y"]:
-            dup_0_1["%s_psfFluxNdata" % f] = 0
-        dup_0_2 = {"ra": original_diaObjects.iloc[0]['ra'],
-                   "dec": original_diaObjects.iloc[0]['dec'] - 0.1/3600,
-                   "diaObjectId": 1002,
-                   "nDiaSources": 1}
-        for f in ["u", "g", "r", "i", "z", "y"]:
-            dup_0_2["%s_psfFluxNdata" % f] = 0
-        dup_0_3 = {"ra": original_diaObjects.iloc[0]['ra'] + 0.2/3600,
-                   "dec": original_diaObjects.iloc[0]['dec'] - 0.1/3600,
-                   "diaObjectId": 1003,
-                   "nDiaSources": 1}
-        for f in ["u", "g", "r", "i", "z", "y"]:
-            dup_0_3["%s_psfFluxNdata" % f] = 0
+        duplicate_rows = []
+        offsets = [
+            (0, [(0, 0.2), (0.1, 0.2), (0, -0.1), (0.2, -0.1)], 1000),
+            (1, [(0, 0.2), (0.1, 0.2), (0, -0.1)], 2000),
+            (2, [(0, 0.2)], 3000),
+        ]
+        for obj_idx, offset_list, base_id in offsets:
+            base_ra = original_diaObjects.iloc[obj_idx]['ra']
+            base_dec = original_diaObjects.iloc[obj_idx]['dec']
+            for i, (dra, ddec) in enumerate(offset_list):
+                row = {"ra": base_ra + dra / 3600,
+                       "dec": base_dec + ddec / 3600,
+                       "diaObjectId": base_id + i,
+                       "nDiaSources": 1}
+                for f in ["u", "g", "r", "i", "z", "y"]:
+                    row["%s_psfFluxNdata" % f] = 0
+                duplicate_rows.append(row)
 
-        dup_1_0 = {"ra": original_diaObjects.iloc[1]['ra'],
-                   "dec": original_diaObjects.iloc[1]['dec'] + 0.2/3600,
-                   "diaObjectId": 2000,
-                   "nDiaSources": 1}
-        for f in ["u", "g", "r", "i", "z", "y"]:
-            dup_1_0["%s_psfFluxNdata" % f] = 0
-        dup_1_1 = {"ra": original_diaObjects.iloc[1]['ra'] + 0.1/3600,
-                   "dec": original_diaObjects.iloc[1]['dec'] + 0.2/3600,
-                   "diaObjectId": 2001,
-                   "nDiaSources": 1}
-        for f in ["u", "g", "r", "i", "z", "y"]:
-            dup_1_1["%s_psfFluxNdata" % f] = 0
-        dup_1_2 = {"ra": original_diaObjects.iloc[1]['ra'],
-                   "dec": original_diaObjects.iloc[1]['dec'] - 0.1/3600,
-                   "diaObjectId": 2002,
-                   "nDiaSources": 1}
-        for f in ["u", "g", "r", "i", "z", "y"]:
-            dup_1_2["%s_psfFluxNdata" % f] = 0
+        duplicate_diaObjects = pd.DataFrame(
+            data=duplicate_rows).set_index("diaObjectId", drop=False)
+        diaObjects = pd.concat([original_diaObjects, duplicate_diaObjects])
 
-        dup_2_0 = {"ra": original_diaObjects.iloc[2]['ra'],
-                   "dec": original_diaObjects.iloc[2]['dec'] + 0.2/3600,
-                   "diaObjectId": 3000,
-                   "nDiaSources": 1}
-        for f in ["u", "g", "r", "i", "z", "y"]:
-            dup_2_0["%s_psfFluxNdata" % f] = 0
+        diaSources = makeDiaSources(
+            100, diaObjects["diaObjectId"].to_numpy(), exposure, rng)
+        diaForcedSources = makeDiaForcedSources(
+            200, diaObjects["diaObjectId"].to_numpy(), exposure, rng)
 
-        data = [dup_0_0, dup_0_1, dup_0_2, dup_0_3,
-                dup_1_0, dup_1_1, dup_1_2,
-                dup_2_0]
-        duplicate_diaObjects = pd.DataFrame(data=data).set_index("diaObjectId", drop=False)
+        storeDateTime = regionTime.timespan.begin.tai - 30 * astropy.units.day
+        apdb.store(storeDateTime, diaObjects, diaSources, diaForcedSources)
 
-        self.diaObjects = pd.concat([original_diaObjects, duplicate_diaObjects])
-
-        self.diaSources = makeDiaSources(
-            100, self.diaObjects["diaObjectId"].to_numpy(), self.exposure, rng)
-        self.diaForcedSources = makeDiaForcedSources(
-            200, self.diaObjects["diaObjectId"].to_numpy(), self.exposure, rng)
-
-        # Store the test diaSources as though they were observed a month before
-        # the current exposure.
-        dateTime = self.regionTime.timespan.begin.tai - 30 * astropy.units.day
-        self.apdb.store(dateTime,
-                        self.diaObjects,
-                        self.diaSources,
-                        self.diaForcedSources)
-
-        # These columns are not in the DPDD, yet do appear in DiaSource.yaml.
-        # We don't need to check them against the default APDB schema.
-        self.ignoreColumns = ["band", "bboxSize", "isDipole", "flags"]
-
-    def _makeConfig(self, **kwargs):
         config = DeduplicateAllSkyDiaObjectsTask.ConfigClass()
-        config.apdb_config_url = self.config_file.name
-        config.earliestMidpointMjdTai = self.dateTime.mjd
-        config.update(**kwargs)
-        return config
+        config.apdb_config_url = config_file.name
+        config.earliestMidpointMjdTai = dateTime.mjd
+        task = DeduplicateAllSkyDiaObjectsTask(config=config)
+
+        return task, apdb, regionTime, dateTime
 
     def testRun(self):
         """Test the full run method for deduplication.
         """
-        diaConfig = self._makeConfig()
-        task = DeduplicateAllSkyDiaObjectsTask(config=diaConfig)
+        task, apdb, regionTime, dateTime = self._make_apdb_with_duplicates()
         result = task.run()
 
         self.assertIsNotNone(result.diaObjectDeduplicationMap)
-
         self.assertEqual(len(result.diaObjectDeduplicationMap), 8)
 
         self.assertIn('removedDiaObjectId', result.diaObjectDeduplicationMap.columns)
@@ -181,11 +139,9 @@ class TestDeduplicateAllSkyDiaObjects(unittest.TestCase):
         kept_ids = set(result.diaObjectDeduplicationMap['keptDiaObjectId'])
         self.assertTrue(kept_ids.issubset({1, 2, 3}))
 
-        # Check that diaSources have been remapped from removed diaObjectIds
-        # (We can't easily compare to the kept ids because non-duplicated ids
-        # are not included in the deduplication map.)
-        updated_diaSources = self.apdb.getDiaSources(region=self.regionTime.region, object_ids=None,
-                                                     visit_time=self.dateTime, start_time=None)
+        updated_diaSources = apdb.getDiaSources(
+            region=regionTime.region, object_ids=None,
+            visit_time=dateTime, start_time=None)
         source_object_ids = set(updated_diaSources['diaObjectId'])
         self.assertTrue(source_object_ids.isdisjoint(removed_ids),
                         "DiaSources should not reference removed diaObjectIds")
@@ -193,10 +149,6 @@ class TestDeduplicateAllSkyDiaObjects(unittest.TestCase):
     def test_remap_clusters(self):
         """Test the cluster remapping logic.
         """
-        diaConfig = self._makeConfig()
-        task = DeduplicateAllSkyDiaObjectsTask(config=diaConfig)
-
-        # Create a simple test DataFrame with cluster labels
         test_data = pd.DataFrame({
             'diaObjectId': [1, 2, 3, 4, 5, 6],
             'validityStartMjdTai': [100.0, 101.0, 102.0, 200.0, 201.0, 300.0],
@@ -205,11 +157,10 @@ class TestDeduplicateAllSkyDiaObjects(unittest.TestCase):
             'dec': [5.0, 5.0, 5.0, 10.0, 10.0, 15.0]
         })
 
-        dedup_map = task.remap_clusters(test_data)
+        dedup_map = self.task.remap_clusters(test_data)
 
-        self.assertEqual(len(dedup_map), 3)  # 2 + 1 remappings
+        self.assertEqual(len(dedup_map), 3)
 
-        # Check that objects 2 and 3 map to object 1
         obj2_mapping = dedup_map[dedup_map['removedDiaObjectId'] == 2]
         obj3_mapping = dedup_map[dedup_map['removedDiaObjectId'] == 3]
         self.assertEqual(len(obj2_mapping), 1)
@@ -217,42 +168,45 @@ class TestDeduplicateAllSkyDiaObjects(unittest.TestCase):
         self.assertEqual(obj2_mapping.iloc[0]['keptDiaObjectId'], 1)
         self.assertEqual(obj3_mapping.iloc[0]['keptDiaObjectId'], 1)
 
-        # Check that object 5 maps to object 4
         obj5_mapping = dedup_map[dedup_map['removedDiaObjectId'] == 5]
         self.assertEqual(len(obj5_mapping), 1)
         self.assertEqual(obj5_mapping.iloc[0]['keptDiaObjectId'], 4)
 
     def test_cluster(self):
-        """Test the clustering algorithm.
+        """Test clustering with known duplicate groups.
         """
-        diaConfig = self._makeConfig(maxClusteringDistance=1.5)
-        task = DeduplicateAllSkyDiaObjectsTask(config=diaConfig)
+        # Three groups of duplicates within 0.3 arcsec, well separated
+        ra = [10.0, 10.0 + 0.1 / 3600, 10.0 + 0.2 / 3600,
+              10.0, 10.0 + 0.2 / 3600,
+              50.0, 50.0 + 0.1 / 3600, 50.0 + 0.2 / 3600,
+              50.0,
+              90.0, 90.0 + 0.2 / 3600]
+        dec = [5.0, 5.0 + 0.2 / 3600, 5.0 - 0.1 / 3600,
+               5.0 + 0.2 / 3600, 5.0 - 0.1 / 3600,
+               20.0, 20.0 + 0.2 / 3600, 20.0 - 0.1 / 3600,
+               20.0 + 0.2 / 3600,
+               40.0, 40.0 + 0.2 / 3600]
+        diaObjects = _make_diaObjects(ra, dec)
+        cluster_labels = self.task.cluster(diaObjects)
 
-        cluster_labels = task.cluster(self.diaObjects)
+        self.assertEqual(len(cluster_labels), len(diaObjects))
 
-        self.assertEqual(len(cluster_labels), len(self.diaObjects))
-
-        group1_ids = [1, 1000, 1001, 1002, 1003]
-        group1_mask = self.diaObjects['diaObjectId'].isin(group1_ids)
-        group1_labels = cluster_labels[group1_mask]
-
+        # Group 1 (indices 0-4) should share a label
+        group1_labels = cluster_labels.iloc[0:5]
         self.assertEqual(len(group1_labels.unique()), 1,
                          "Objects in duplicate group 1 should share a cluster label")
 
-        group2_ids = [2, 2000, 2001, 2002]
-        group2_mask = self.diaObjects['diaObjectId'].isin(group2_ids)
-        group2_labels = cluster_labels[group2_mask]
-
+        # Group 2 (indices 5-8) should share a label
+        group2_labels = cluster_labels.iloc[5:9]
         self.assertEqual(len(group2_labels.unique()), 1,
                          "Objects in duplicate group 2 should share a cluster label")
 
-        group3_ids = [3, 3000]
-        group3_mask = self.diaObjects['diaObjectId'].isin(group3_ids)
-        group3_labels = cluster_labels[group3_mask]
-
+        # Group 3 (indices 9-10) should share a label
+        group3_labels = cluster_labels.iloc[9:11]
         self.assertEqual(len(group3_labels.unique()), 1,
                          "Objects in duplicate group 3 should share a cluster label")
 
+        # Groups should have different labels
         self.assertNotEqual(group1_labels.iloc[0], group2_labels.iloc[0],
                             "Group 1 and Group 2 should have different cluster labels")
         self.assertNotEqual(group1_labels.iloc[0], group3_labels.iloc[0],
@@ -263,29 +217,23 @@ class TestDeduplicateAllSkyDiaObjects(unittest.TestCase):
     def test_count_duplicates(self):
         """Test the duplicate counting method.
         """
-        diaConfig = self._makeConfig(maxClusteringDistance=1.5)
-        task = DeduplicateAllSkyDiaObjectsTask(config=diaConfig)
-
         test_catalog = pd.DataFrame({
             'diaObjectId': [1, 2, 3, 4, 5],
-            'ra': [10.0, 10.0001,  # Pair 1
-                   10.1, 10.1001,  # Pair 2
-                   10.3],          # Single
+            'ra': [10.0, 10.0001,
+                   10.1, 10.1001,
+                   10.3],
             'dec': [5.0, 5.0001,
                     5.1, 5.1001,
                     5.3]
         })
 
-        duplicate_count = task.count_duplicates(test_catalog)
-
+        duplicate_count = self.task.count_duplicates(test_catalog)
         self.assertEqual(duplicate_count, 4)
 
-        # Test with custom radius
-        duplicate_count_small = task.count_duplicates(test_catalog, radius_arcsec=0.1)
+        duplicate_count_small = self.task.count_duplicates(test_catalog, radius_arcsec=0.1)
         self.assertEqual(duplicate_count_small, 0)
 
-        # Test with large radius
-        duplicate_count_large = task.count_duplicates(test_catalog, radius_arcsec=3600.0)
+        duplicate_count_large = self.task.count_duplicates(test_catalog, radius_arcsec=3600.0)
         self.assertEqual(duplicate_count_large, 5)
 
 
@@ -450,7 +398,6 @@ class TestClusterSmallDataset(unittest.TestCase):
         self.assertIsInstance(labels, pd.Series)
         self.assertTrue(labels.index.equals(diaObjects.index))
         self.assertEqual(labels.name, "cluster_label")
-
 
     def test_large_cluster(self):
         """A tight group of many objects should mostly cluster together.
